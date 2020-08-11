3bcbb2d835415fedf6952ef3f60c26cac606c455
Ammon Jeffery <savingphace@gmail.com>
2020-08-10 21:53:15 -0600

    added initial requirements to index, started connection, started startTracker()

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-let mysql = require("mysql");
-let inquirer = require("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table")
 
-let connection = mysql.createConnection
+let connection = mysql.createConnection({
+    host: "localhost",
+
+    port:8080,
+
+    user: "root",
+
+    password:"",
+    database:"employee_trackerDB"
+})
+
+connection.connect((err) => {
+    if (err) throw err;
+    startTracker();
+})
+
+function startTracker() {
+    inquirer.prompt({
+        
+    })
+}
